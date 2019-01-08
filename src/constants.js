c419a52a850511fedcbaf690fd120208c519b613
David Luis Wiegandt <davidwiegandt@outlook.com>
2019-01-08 13:41:20 +0100

    Revised  .gitignore

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EXP = 10;
-exports.MIN = 0; // inclusive
-exports.MAX = 1 << exports.EXP; // exclusive
-exports.WILDCARD = 1;
-//# sourceMappingURL=constants.js.map
