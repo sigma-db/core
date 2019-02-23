0aed5fc208d915b7da9285dd18a5812f45fe9036
David Luis Wiegandt <davidwiegandt@outlook.com>
2019-02-23 12:34:15 +0100

    code cleanup

--- lib/query/evaluation/cds.ts
+++ lib/query/evaluation/cds.ts
@@ -19,29 +19,30 @@ export class CDS {
         }
     }
 
+    private cover(box: ArrayLike<bigint>, dim: number, trie: DyadicTrie<any>): Array<bigint> {
+        if (dim == box.length - 1) {
+            const b = trie.search(box[dim]);
+            if (!!b) {
+                const [int,] = b;
+                return [int];
+            }
+        } else {
+            for (let [int, _trie] of trie.searchAll(box[dim])) {
+                const b = this.cover(box, dim + 1, _trie);
+                if (!!b) {
+                    b.unshift(int);
+                    return b;
+                }
+            }
+        }
+    }
+
     /**
      * Checks whether a given box is contained in any box in the CDS and returns a witness if so
      * @param box The box to check
      */
     public witness(box: Box): Box {
-        const cover = (dim: number, trie: DyadicTrie<any>): bigint[] => {
-            if (dim == box.length - 1) {
-                const b = trie.search(box[dim]);
-                if (!!b) {
-                    const [int,] = b;
-                    return [int];
-                }
-            } else {
-                for (let [int, _trie] of trie.searchAll(box[dim])) {
-                    const b = cover(dim + 1, _trie);
-                    if (!!b) {
-                        return [int, ...b];
-                    }
-                }
-            }
-        };
-
-        const b = cover(0, this.data);
+        const b = this.cover(box, 0, this.data);
         if (!!b) {
             return Box.from(b);
         }
