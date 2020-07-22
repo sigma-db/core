f1f84f0f68d97ec7ee27aabd86bbefbea5ff2391
David Luis Wiegandt <wigandtd@hu-berlin.de>
2020-07-22 18:16:19 +0200

    extended public API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-﻿export { Instance, Relation } from "./database";
+﻿export { Instance, Relation, Attribute, DataType } from "./database";
 export { Parser, Statement, StatementType } from "./parser";
 export { Engine, EngineType, Result, ResultType } from "./engine";
